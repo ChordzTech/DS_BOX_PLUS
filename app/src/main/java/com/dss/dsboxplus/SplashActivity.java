539830b5600d9f0f8001f3571656daddb1cd1c9d
Amit <amitdbansode@gmail.com>
2023-10-27 17:41:24 +0530

    lottie animation added on SplashActivity

--- app/src/main/java/com/dss/dsboxplus/SplashActivity.java
+++ app/src/main/java/com/dss/dsboxplus/SplashActivity.java
@@ -7,19 +7,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.dss.dsboxplus.databinding.ActivityMainBinding;
+import com.dss.dsboxplus.databinding.ActivitySplashBinding;
 
 
-public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding mainBinding;
+public class SplashActivity extends AppCompatActivity {
+    ActivitySplashBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        mainBinding= DataBindingUtil.setContentView(this,R.layout.activity_splash);
         mainBinding.ivCompanyLogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent=new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
