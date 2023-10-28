166f21a2588105bc2d0cd4a7377617a579204f6b
Pankaj <pankajkashid12@gmail.com>
2023-10-27 22:56:24 -0700

    recycler view added in clients fragment

---
+++ app/src/main/java/com/dss/dsboxplus/model/ClientsViewModel.java
@@ -0,0 +1,47 @@
+package com.dss.dsboxplus.model;
+
+public class ClientsViewModel {
+    String imageURL;
+    String nameOfClient;
+    String phoneNumberOfClient;
+    String numberOfEstimatesOfClients;
+
+    public ClientsViewModel(String imageURL, String nameOfClient, String phoneNumberOfClient, String numberOfEstimatesOfClients) {
+        this.imageURL = imageURL;
+        this.nameOfClient = nameOfClient;
+        this.phoneNumberOfClient = phoneNumberOfClient;
+        this.numberOfEstimatesOfClients = numberOfEstimatesOfClients;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getNameOfClient() {
+        return nameOfClient;
+    }
+
+    public void setNameOfClient(String nameOfClient) {
+        this.nameOfClient = nameOfClient;
+    }
+
+    public String getPhoneNumberOfClient() {
+        return phoneNumberOfClient;
+    }
+
+    public void setPhoneNumberOfClient(String phoneNumberOfClient) {
+        this.phoneNumberOfClient = phoneNumberOfClient;
+    }
+
+    public String getNumberOfEstimatesOfClients() {
+        return numberOfEstimatesOfClients;
+    }
+
+    public void setNumberOfEstimatesOfClients(String numberOfEstimatesOfClients) {
+        this.numberOfEstimatesOfClients = numberOfEstimatesOfClients;
+    }
+}
