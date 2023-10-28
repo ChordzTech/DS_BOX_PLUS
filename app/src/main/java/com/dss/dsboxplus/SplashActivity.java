c32273c4b709051e410c27948ab942418932bba1
Amit <amitdbansode@gmail.com>
2023-10-28 11:04:39 +0530

    Splash screen updated

--- app/src/main/java/com/dss/dsboxplus/SplashActivity.java
+++ app/src/main/java/com/dss/dsboxplus/SplashActivity.java
@@ -1,28 +1,32 @@
 package com.dss.dsboxplus;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.view.View;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.dss.dsboxplus.databinding.ActivitySplashBinding;
 
 
 public class SplashActivity extends AppCompatActivity {
     ActivitySplashBinding mainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainBinding= DataBindingUtil.setContentView(this,R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
 
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Intent intent=new Intent(SplashActivity.this, LoginActivity.class);
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
             startActivity(intent);
-        },3000);
+            finish();
+        }, 3000);
 
        /* mainBinding.ivCompanyLogo.setOnClickListener(new View.OnClickListener() {
             @Override
