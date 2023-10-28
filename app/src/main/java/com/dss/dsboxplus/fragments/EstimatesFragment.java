6865bdaacf1d24995cd6a7db9d9f03d02efab6f8
Amit <amitdbansode@gmail.com>
2023-10-28 17:53:25 +0530

    UI fixes for esitmate item and client item

--- app/src/main/java/com/dss/dsboxplus/fragments/EstimatesFragment.java
+++ app/src/main/java/com/dss/dsboxplus/fragments/EstimatesFragment.java
@@ -23,15 +23,8 @@ import java.util.ArrayList;
  * create an instance of this fragment.
  */
 public class EstimatesFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
     private RecyclerView rvEstimatesRecyclerView;
     private EstimatesViewAdapter estimatesViewAdapter;
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public EstimatesFragment() {
         // Required empty public constructor
@@ -47,7 +40,6 @@ public class EstimatesFragment extends Fragment {
     public static EstimatesFragment newInstance() {
         EstimatesFragment fragment = new EstimatesFragment();
         Bundle args = new Bundle();
-
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +48,6 @@ public class EstimatesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
