e15a5f9da72a2b028a92a8ec2a7b2b45916e4450
Pankaj <pankajkashid12@gmail.com>
2024-03-18 18:19:12 +0530

    bug fixes

--- app/src/main/java/com/dss/dsboxplus/loginandverification/SplashActivity.java
+++ app/src/main/java/com/dss/dsboxplus/loginandverification/SplashActivity.java
@@ -94,7 +94,7 @@ public class SplashActivity extends BaseActivity {
                 Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
             } else if (userDetailsResponse.getCode() == 400) {
-                showMessagePopUp("Your account is temporarily blocked. Please contact Admin.");
+                showMessagePopUp("Your mobile number already registered with another device, please contact support to deauthorize another device.");
             } else {
                 if (userDetailsResponse.getData() != null && userDetailsResponse.getData().get(0).getAndroidid().equalsIgnoreCase("NewUser")) {
                     Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
