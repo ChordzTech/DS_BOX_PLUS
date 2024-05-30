c14aee44c4fed23e6b206e67659d6455989e0257
Pankaj <pankajkashid12@gmail.com>
2024-05-30 21:22:10 +0530

    Bug fixes

--- app/src/main/java/com/dss/dsboxpro/clients/EstimateListActivity.java
+++ app/src/main/java/com/dss/dsboxpro/clients/EstimateListActivity.java
@@ -96,13 +96,13 @@ public class EstimateListActivity extends BaseActivity implements EstimatesViewA
             }
         });
 
-        binding.fabAddEstimateInEstimateList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(EstimateListActivity.this, NewEstimateActivity.class);
-                startActivity(intent);
-            }
-        });
+//        binding.fabAddEstimateInEstimateList.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(EstimateListActivity.this, NewEstimateActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
     private void prepareData() {
         Collections.sort(estimateList, new Comparator<DataItem>() {
